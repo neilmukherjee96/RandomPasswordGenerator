03cc466813453693783afab7dab887a095af66ca
neilmukherjee96 <neilmukherjee96@gmail.com>
2021-02-24 08:51:23 -0500

    add exceptions where button can be clicked after eentering length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  includeLetters=false;
-  includeNumbers=false;
-  includeSymbols=false;
-  password='';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
 
-  onButtonClick(){
-    console.log(this.includeLetters);
-    console.log(this.includeNumbers);
-    console.log(this.includeSymbols);
-    this.password='MY PASSWORD!!!';
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
-  onChangeUseLetters(){
-    this.includeLetters=!this.includeLetters;
-  } 
-  onChangeUseNumbers(){
-    this.includeNumbers=!this.includeNumbers;
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
   }
-  onChangeUseSymbols(){
-    this.includeSymbols=!this.includeSymbols;
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 
-  
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword='';
+    for(let i=0;i<this.length;i++){
+      const index=Math.floor(Math.random()*validChars.length)
+      generatedPassword+=validChars[index];
+    }
+    this.password=generatedPassword;
+  }
 }
